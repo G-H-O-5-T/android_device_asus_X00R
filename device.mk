4d4b270ade0f0068e958ae3d3d934a5f73936ff9
iamsaalim <saalimquadri1@gmail.com>
2020-07-11 08:10:54 +0000

    X00R : Import sensor configs * from UL-ASUS_X00P-WW-15.00.2005.120-user
    Change-Id: Ifbce690dee8535569d1f289bc499857877dc8aaa

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ $(call inherit-product-if-exists, vendor/asus/X00R/X00R-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # Inherit from msm8937-common
 $(call inherit-product, device/asus/msm8937-common/msm8937.mk)
